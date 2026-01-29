7a4189e8905c909cb2b1fc04c964c2f96b67bc9f
xSPx <work.xSPx@gmail.com>
2026-01-29 19:53:05 +0200

    add base files and folders

---
+++ __manifest__.py
@@ -0,0 +1,31 @@
+{
+    "name": "Hospital visit",
+    "summary": "Odoo module for recording patient visits",
+    "version": "19.0.1.0.1",
+    "category": "Services",
+    "website": "https://github.com/git-xSPx/hr_hospital",
+    "author": "Serhii Pidopryhora",
+    "license": "GPL-2",
+
+    'depends': [
+        'base',
+    ],
+
+    'external_dependencies': {
+        'python': [],
+    },
+
+
+    "data": [
+    ],
+    "demo": [
+    ],
+
+    "installable": True,
+    'auto_install': False,
+
+    'images': [
+        'static/description/icon.png'
+    ],
+
+}
