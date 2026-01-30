be4852935ab60974b9848d75dd557b172c6dc536
xSPx <work.xSPx@gmail.com>
2026-01-31 01:52:52 +0200

    fix error

--- __manifest__.py
+++ __manifest__.py
@@ -2,7 +2,7 @@
     "name": "Hospital visit",
     "summary": "Odoo module for recording patient visits",
     "version": "19.0.1.4.1",
-    "category": "Services",
+    "category": "Human Resources",
     "website": "https://github.com/git-xSPx/hr_hospital",
     "author": "Serhii Pidopryhora",
     "license": "GPL-2",
@@ -14,6 +14,7 @@
     "data": [
         'security/ir.model.access.csv',
         'data/hr_hospital_disease_data.xml',
+        'views/hr_hospital_disease_view.xml',
         'views/hr_hospital_patient_view.xml',
         'views/hr_hospital_doctor_view.xml',
         'views/hr_hospital_visit_view.xml',
